639e5494f10a9a2e1ef0c0b86080e24d9d43cd4e
Parsa Hassani <mohamad.parsa.hasani@gmail.com>
2019-11-11 10:44:16 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#number.sh
+#Parsa Hassani
+
+echo "Enter a positive number"
+read NUMBER
+N=1
+
+while [ $N -le $NUMBER ]
+do
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo "$N Even"
+	else
+		echo "$N Odd"
+	fi
+	N=$[$N+1]
+done
